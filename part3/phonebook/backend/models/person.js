a9442f759b43b679ead4f2049b6e2252aa994200
Andrii Yakovliev <andrii.iakovliev@gmail.com>
2026-01-09 18:21:25 +0200

    feat: complete homework tasks 3.19-3.21

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -1,30 +1,30 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to:', url);
+console.log('connecting to:', url)
 
 mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch(error => {
-        console.log('error connectint to MongoDB', error.message);
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connectint to MongoDB', error.message)
+  })
 
 const personsSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 personsSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Person = mongoose.model('Persons', personsSchema)
